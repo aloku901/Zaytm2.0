4d0a68514fbd02854c9522172004e2fe9d5b4ed6
Alok <aloku901@gmail.com>
2024-07-09 23:12:49 +0530

    Add Next auth add

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,13 @@
+"use client";
+import { useBalance } from "@repo/store/useBalance";
 import { PrismaClient } from "@repo/db/client";
 const client = new PrismaClient();
 
 export default function Page(): JSX.Element {
+  const balance = useBalance();
   return(
     <div className="text-3xl text-red-800 bg-white">
-      Alok Upadhyay
+      Alok Upadhyay {balance}
     </div>
   );
  }
